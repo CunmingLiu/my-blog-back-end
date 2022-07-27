0bd7c3fa33ebc701e4b4ccf86b7e03c5a2fe697f
ChamanLiu <78123192+CunmingLiu@users.noreply.github.com>
2022-07-27 16:08:05 +0800

    first commit

---
+++ app/service/systems/login.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { Service } = require('egg');
+
+class LoginService extends Service {
+  async index(params) {
+    const { ctx, app } = this;
+    const db = await ctx.model.User.find(params);
+    console.log('params', params, db);
+    db.push(params);
+    if (db.length !== 0) {
+      const token = app.jwt.sign(
+        {
+          username: ctx.request.body.username,
+        },
+        app.config.jwt.secret
+      );
+      return token;
+    }
+    return false;
+  }
+}
+
+module.exports = LoginService;
